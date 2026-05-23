9073cfe8df3295419ce6906c42747697778fd11d
Meghna-97 <meghanaraman9@gmail.com>
2026-05-23 16:19:25 +0530

    fix basename for github pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import Editor from './pages/Editor';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/collab-documents">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/document/:id" element={<Editor />} />
